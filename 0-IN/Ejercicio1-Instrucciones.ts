2f50978059c3c90f791951405ae0cca238ee25f5
Julian <julianleo92@hotmail.com>
2022-05-15 18:48:27 -0300

    Primer ejercicio de console.log

---
+++ 0-IN/Ejercicio1-Instrucciones.ts
@@ -0,0 +1,31 @@
+console.log("Instrucciones para conducir un automovil:");
+console.log("")
+console.log("Antes de la puesta en marcha, verificar: ");
+console.log("")
+console.log("   -Que la palanca de cambio este en punto muerto");
+console.log("   -Que los espejos retrovisores esten alineados a la linea de visión del conductor");
+console.log("   -Que el freno de mano este accionado");
+console.log("   -Colocarse el cinturon de seguridad");
+console.log("")
+console.log("-Girar la llave para poner en contacto y luego girar para dar arranque");
+console.log("-Encender las luces");
+console.log("-Para comenzar a moverse quitar freno de mano");
+console.log("-Luego presionar el embrague, poner primera marcha y acelerar");
+console.log("")
+console.log("Conducir con precaución y respetar las normas de transito");
+console.log("")
+console.log("")
+console.log("Lista de códigos ASCII útiles:")
+console.log("")
+console.log("-ALT+123= {")
+console.log("-ALT+125= }")
+console.log("-ALT+64=  @ ")
+console.log("-ALT+35=  #")
+console.log("-ALT+33=  !")
+console.log("-ALT+95=  _")
+console.log("-ALT+42=  *")
+console.log("-ALT+126= ~")
+console.log("-ALT+45=  -")
+console.log("-ALT+91= [")
+console.log("-ALT+93= ]")
+
