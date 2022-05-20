42182413ae09092c2c5a17582228c4c18f281c69
Julian <julianleo92@hotmail.com>
2022-05-19 22:26:31 -0300

    Prueba de funcionamiento

---
+++ 1-PB/Clase_4/ejercicioTriangulo.ts
@@ -0,0 +1,18 @@
+/*Realice un programa que devuelva el area del triángulo usando los siguientes pares de base-altura
+(1,2) (2.4) (3.6) (4.8) (5.10) (6.12) (7.14)
+
+Implementar un metodo llamado calcularAreaTriangulo que reciba dos numeros por parámetro (uno llamado base y el otro altura)*/
+
+import * as rls from 'readline-sync';
+
+console.log('Ingresar un valor para la base y un valor para la altura de los siguientes pares: (1,2) (2.4) (3.6) (4.8) (5.10) (6.12) (7.14)')
+
+let i:number;
+
+for (i=1; i<=50; i++)
+console.log ('El area es =', calcularAreaTriangulo(i,i*2))
+
+function calcularAreaTriangulo (base:number, altura:number): number{
+    return (base*altura/2);
+
+}
